19fc95ab0e89705299902dd0e8b0117a7e1d0ab5
José Castro <a.j.castro.b@gmail.com>
2026-09-02 02:33:06 -0500

    ✨ [FEAT] Se creó un script para dar citas aleatorias en la sección de fragmentos.

---
+++ src/lib/getQuotes.js
@@ -0,0 +1,14 @@
+import quotes from "./quotes.json" with { type: "json" };
+
+function getQuotes(arr) {
+  const copy = [...arr];
+
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+
+  return copy.slice(0, 2);
+}
+
+export const fragments = getQuotes(quotes);
